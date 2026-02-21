6bde37675e9ae4d9764e794371e5353618185414
Luke Manning <luketmanning@gmail.com>
2026-02-21 23:16:58 +0000

    feat: US-001 - CLI Package Structure

---
+++ packages/cli/eslint.config.js
@@ -0,0 +1,24 @@
+import tseslint from '@typescript-eslint/eslint-plugin'
+import tsParser from '@typescript-eslint/parser'
+
+export default [
+  {
+    files: ['src/**/*.ts'],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module'
+      }
+    },
+    plugins: {
+      '@typescript-eslint': tseslint
+    },
+    rules: {
+      '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+      '@typescript-eslint/explicit-function-return-type': 'off',
+      '@typescript-eslint/explicit-module-boundary-types': 'off',
+      'no-console': 'off'
+    }
+  }
+]
---
+++ packages/cli/src/cli.ts
@@ -0,0 +1,18 @@
+#!/usr/bin/env node
+
+import { Command } from 'commander'
+import { init } from './commands/init.js'
+
+const program = new Command()
+
+program
+  .name('folio')
+  .description('CLI tool for Folio project showcase pages')
+  .version('0.0.0')
+
+program
+  .command('init')
+  .description('Initialize a new Folio project')
+  .action(init)
+
+program.parse()
